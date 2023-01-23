69c097c30745ce957108470a57563616509e0ef9
oskar <ne0h747@gmial.com>
2023-01-23 22:51:10 +0100

    Added support of Excel format duration base

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import os
 import csv
+import xlrd
 
 def loadmcrlists():
     mcrlists_list = []
@@ -31,16 +32,36 @@ def framediff(timecode1, timecode2):
     return abs(diff)
 
 
-def load_durationdb(durationDB_url):
+def load_durationdb_csv(durationDB_url):
     duration_base = {}
     with open(durationDB_url) as durationDB:
         next(durationDB)
         for row in csv.reader(durationDB, delimiter=','):
-            if len(row) >= 2 and len(row[1]) == 11:
+            if len(row) >= 2 and len(row[1]) == 11 and len(row[0]) >= 7:
                 duration_base[row[0]] = row[1]
             else:
                 continue
-        return duration_base
+    return duration_base
+
+def load_durationdb_excel(durationDB_url):
+    duration_base = {}
+    book = xlrd.open_workbook(durationDB_url)
+    sh = book.sheet_by_index(0)
+    komorka = sh.cell_value
+    for row in range(sh.nrows):
+        mediaID = komorka(row, 0)
+        duration = komorka(row, 1)
+        if len(mediaID) >= 7 and len(duration) == 11:
+            duration_base[mediaID] = duration
+    return duration_base
+
+def load_durationdb(durationDB_url):
+    if durationDB_url.endswith(".xlsx") or durationDB_url.endswith(".xls"):
+        duration_base = load_durationdb_excel(durationDB_url)
+    if durationDB_url.endswith(".csv"):
+        duration_base = load_durationdb_csv(durationDB_url)
+    return duration_base
+
 
 def load_playlist(playlist_url):
     playlist = {}
@@ -80,7 +101,12 @@ def check_playlist(playlist, duration_base):
 wrongdur_dict = {}
 notinDB_dict = {}
 correct_dict = {}
-durationDB_url = 'ExampleDurationBase.csv'
+durationDB_url = input('Podaj ścieżkę bazy czasów:\n')
+
+if len(durationDB_url) <= 4:
+    durationDB_url = 'ExampleDurationBase.csv'
+    print(f"Nie wybrano bazy czasów, używam domyślnej: {durationDB_url}")
+
 duration_base = load_durationdb(durationDB_url)
 
 for mcrlist in loadmcrlists():
@@ -90,10 +116,13 @@ for mcrlist in loadmcrlists():
     if result == True:
         print(f"Playlista jest ok!")
 
-print('\n\nPoniższe MediaID mają zły czas:')
-for wpis in wrongdur_dict:
-    print(f'{wpis} {wrongdur_dict.get(wpis)} zamiast {duration_base.get(wpis)}')
-print('\n\nPoniższych MediaID nie ma w bazie:')
-for wpis in notinDB_dict:
-    print(wpis, notinDB_dict.get(wpis))
+if len(wrongdur_dict) > 0:
+    print('\n\nPoniższe MediaID mają zły czas:')
+    for wpis in wrongdur_dict:
+        print(f'{wpis} {wrongdur_dict.get(wpis)} zamiast {duration_base.get(wpis)}')
+
+if len(notinDB_dict) > 0:
+    print('\n\nPoniższych MediaID nie ma w bazie:')
+    for wpis in notinDB_dict:
+        print(wpis, notinDB_dict.get(wpis))
 input()
